f5748be5827f06bcc7eeab78e15156f05a9113bb
hxntxfr <vladmura42@gmail.com>
2026-03-14 14:34:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,5 +22,5 @@
       if (aboutSection) aboutSection.style.opacity = '1';
     });
 
-    document.querySelector('.copyright').innerHTML = `<i class="far fa-copyright"></i> ${new Date().getFullYear()} mono::work — all grey areas reserved`;
+    document.querySelector('.copyright').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M14.83 14.83a4 4 0 1 1 0-5.66"/></g></svg> ${new Date().getFullYear()} mono::work — все серые зоны закрашены.`;
   })();
